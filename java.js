13c9e30643e8019cc2ef8b372b3efe5e2fc15a2e
Jaigupta <jaig29012@gmail.com>
2025-07-17 11:04:51 +0530

    Update java.js

--- java.js
+++ java.js
@@ -1,46 +1,14 @@
-let nextBtn = document.querySelector("#next");
-let prevBtn = document.querySelector("#prev");
-let slides = document.querySelectorAll(".slide");
-let changeSlide = 0;
-// console.log(changeSlide);
-nextBtn.addEventListener("click", function() {
-  
-    slides.forEach(function (slide, index) {
-    if (slide.classList.contains("show") === true) {
-      changeSlide = index + 1;
-      slide.classList.remove("show");
-    }
-    
-  });
-//   console.log(changeSlide);
-  if (changeSlide < slides.length) {
-    slides[changeSlide].classList.add("show");
-    }
-  else {
-      changeSlide = 0;
-      slides[changeSlide].classList.add("show");
-    }
-});
-// console.log(changeSlide);
-prevBtn.addEventListener('click', function () {
-   
-    slides.forEach(function (slide, index) {
-        if (slide.classList.contains("show") === true) {
-            changeSlide = index - 1;
-            slide.classList.remove("show");
-        }
-       
-        
+ const swiper = new Swiper(".mySwiper", {
+      loop: true,
+      slidesPerView: 1,
+      centeredSlides: true,
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
     });
-    // console.log(changeSlide);
-
-    if (changeSlide < slides.length && changeSlide > -1) {
-        slides[changeSlide].classList.add("show");
-    }
-    else {
-        // console.log(slides.length);
-        
-        changeSlide = slides.length - 1;
-        slides[changeSlide].classList.add("show");
-    }
-});
